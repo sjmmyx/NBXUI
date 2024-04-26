8a7a4ff74b0d8272365dfd62a68aab06b93560f8
sjmmyx <47154256+sjmmyx@users.noreply.github.com>
2024-04-26 14:11:55 +0800

    Update nb.sh

--- nb.sh
+++ nb.sh
@@ -1,29 +1,89 @@
 #!/bin/bash
+#
+#   Dante Socks5 Server AutoInstall
+#   -- Owner:       https://www.inet.no/dante
+#   -- Provider:    https://sockd.info
+#   -- Author:      Lozy
+#
 
-# 安装必需的软件包
-packages=("openssh-server" "sshpass")
+# Check if user is root
+if [ $(id -u) != "0" ]; then
+    echo "Error: You must be root to run this script, please use root to install"
+    exit 1
+fi
 
-for pkg in "${packages[@]}"; do
-    if ! dpkg -s "$pkg" &> /dev/null; then
-        echo "Installing $pkg..."
-        sudo apt-get update
-        sudo apt-get install -y "$pkg"
-        echo "$pkg installed successfully."
-    fi
+# Default values
+PORT="1080"
+USER=""
+PASSWD=""
+
+# Function to show usage
+usage() {
+    echo "Usage: $0 [--port <port>] [--user <username>] [--passwd <password>]"
+    exit 1
+}
+
+# Parse command line arguments
+while [ "$#" -gt 0 ]; do
+    case "$1" in
+        --port)
+            PORT="$2"
+            shift 2
+            ;;
+        --user)
+            USER="$2"
+            shift 2
+            ;;
+        --passwd)
+            PASSWD="$2"
+            shift 2
+            ;;
+        *)
+            usage
+            ;;
+    esac
 done
 
-# 配置 SSH 服务
-sudo sed -i '/^#PasswordAuthentication yes/c\PasswordAuthentication yes' /etc/ssh/sshd_config
-sudo systemctl restart sshd
+REQUEST_SERVER="https://raw.github.com/Lozy/danted/master"
+SCRIPT_SERVER="https://public.sockd.info"
+SYSTEM_RECOGNIZE=""
 
-# 启动 SOCKS5 代理服务器
-echo "Starting SOCKS5 Proxy Server..."
-sudo -u $USER nohup sshpass -p admin ssh -N -D 7777 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null localhost &> /dev/null &
-echo "SOCKS5 Proxy Server started on port 7777."
+[ "$1" == "--no-github" ] && REQUEST_SERVER=${SCRIPT_SERVER}
 
-# 设置 SOCKS5 代理服务器开机自动启动
-echo "Setting up SOCKS5 Proxy Server to start automatically on boot..."
-sudo cp "$PWD/socks5_startup.sh" /etc/init.d/
-sudo chmod +x /etc/init.d/socks5_startup.sh
-sudo update-rc.d socks5_startup.sh defaults
-echo "SOCKS5 Proxy Server will now start automatically on boot."
+if [ -s "/etc/os-release" ];then
+    os_name=$(sed -n 's/PRETTY_NAME="\(.*\)"/\1/p' /etc/os-release)
+
+    if [ -n "$(echo ${os_name} | grep -Ei 'Debian|Ubuntu' )" ];then
+        printf "Current OS: %s\n" "${os_name}"
+        SYSTEM_RECOGNIZE="debian"
+
+    elif [ -n "$(echo ${os_name} | grep -Ei 'CentOS')" ];then
+        printf "Current OS: %s\n" "${os_name}"
+        SYSTEM_RECOGNIZE="centos"
+    else
+        printf "Current OS: %s is not support.\n" "${os_name}"
+    fi
+elif [ -s "/etc/issue" ];then
+    if [ -n "$(grep -Ei 'CentOS' /etc/issue)" ];then
+        printf "Current OS: %s\n" "$(grep -Ei 'CentOS' /etc/issue)"
+        SYSTEM_RECOGNIZE="centos"
+    else
+        printf "+++++++++++++++++++++++\n"
+        cat /etc/issue
+        printf "+++++++++++++++++++++++\n"
+        printf "[Error] Current OS: is not available to support.\n"
+    fi
+else
+    printf "[Error] (/etc/os-release) OR (/etc/issue) not exist!\n"
+    printf "[Error] Current OS: is not available to support.\n"
+fi
+
+if [ -n "$SYSTEM_RECOGNIZE" ];then
+    wget -qO- --no-check-certificate ${REQUEST_SERVER}/install_${SYSTEM_RECOGNIZE}.sh | \
+        bash -s -- --port "$PORT" --user "$USER" --passwd "$PASSWD" | tee /tmp/danted_install.log
+else
+    printf "[Error] Installing terminated"
+    exit 1
+fi
+
+exit 0
