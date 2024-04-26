f5b4f03d4dee555339dbaca69b1e9f606a54c8a7
sjmmyx <47154256+sjmmyx@users.noreply.github.com>
2024-04-26 14:05:28 +0800

    Update nb.sh

--- nb.sh
+++ nb.sh
@@ -1,29 +1,25 @@
 #!/bin/bash
 
-# 安装必需的软件包
-packages=("openssh-server" "sshpass")
+# 更新apt软件包索引
+sudo apt update
 
-for pkg in "${packages[@]}"; do
-    if ! dpkg -s "$pkg" &> /dev/null; then
-        echo "Installing $pkg..."
-        sudo apt-get update
-        sudo apt-get install -y "$pkg"
-        echo "$pkg installed successfully."
-    fi
-done
+# 安装socks5代理软件和依赖
+sudo apt install -y dante-server
 
-# 配置 SSH 服务
-sudo sed -i '/^#PasswordAuthentication yes/c\PasswordAuthentication yes' /etc/ssh/sshd_config
-sudo systemctl restart sshd
+# 配置socks5代理
+echo "logoutput: /var/log/socks.log
+internal: eth0 port = 7777
+external: eth0
+socksmethod: username" | sudo tee /etc/danted.conf
 
-# 启动 SOCKS5 代理服务器
-echo "Starting SOCKS5 Proxy Server..."
-sudo -u $USER nohup sshpass -p admin ssh -N -D 7777 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null localhost &> /dev/null &
-echo "SOCKS5 Proxy Server started on port 7777."
+# 添加socks5代理用户admin，密码为admin
+sudo useradd --shell /usr/sbin/nologin admin
+echo "admin:admin" | sudo chpasswd
 
-# 设置 SOCKS5 代理服务器开机自动启动
-echo "Setting up SOCKS5 Proxy Server to start automatically on boot..."
-sudo cp "$PWD/socks5_startup.sh" /etc/init.d/
-sudo chmod +x /etc/init.d/socks5_startup.sh
-sudo update-rc.d socks5_startup.sh defaults
-echo "SOCKS5 Proxy Server will now start automatically on boot."
+# 启动socks5代理服务
+sudo systemctl start danted
+
+# 设置socks5代理服务开机自启动
+sudo systemctl enable danted
+
+echo "socks5代理已启动，用户名: admin, 密码: admin, 端口: 7777"
